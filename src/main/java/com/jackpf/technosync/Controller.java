ebc3d198b61303208e7622e6ab6749330f117286
Jack Farrelly <jack.farrelly@deepskyblue.com>
2016-01-26 11:39:32 +0000

    Working UI

---
+++ src/main/java/com/jackpf/technosync/Controller.java
@@ -0,0 +1,69 @@
+package com.jackpf.technosync;
+
+import TrackAnalyzer.TrackAnalyzer;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by jackfarrelly on 25/01/2016.
+ */
+public class Controller {
+    private Stage stage;
+
+    private Executor ffmpeg, rubberband;
+
+    @FXML
+    private TableView<Info> tracks;
+
+    @FXML
+    private TableColumn bpm;
+
+    public void initialise(Stage stage) {
+        this.stage = stage;
+        bpm.setCellFactory(TextFieldTableCell.<Info>forTableColumn());
+
+        ffmpeg = new Executor("ffmpeg");
+        rubberband = new Executor("rubberband");
+    }
+
+    protected void addTrack(String filename) throws Exception {
+        TrackAnalyzer analyzer = new TrackAnalyzer(new String[]{filename, "-w", "-o", "/tmp/trackanalyzer.txt"});
+        TrackAnalyzer.Info info = analyzer.analyzeTrack(filename, false);
+
+        ObservableList<Info> data = tracks.getItems();
+        Info trackInfo = new Info(info.filename, Integer.toString(info.bpm));
+        data.add(trackInfo);
+    }
+
+    @FXML
+    protected void addFiles(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Audio files", new ArrayList<String>(Arrays.asList("*.mp3", "*.wav")));
+        fileChooser.getExtensionFilters().add(extFilter);
+        List<File> files = fileChooser.showOpenMultipleDialog(stage);
+        for (File file : files) {
+            try {
+                addTrack(file.getAbsolutePath());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    protected void run(ActionEvent event) {
+        //ffmpeg.run(new String[]{"-i", info.filename, "/tmp/tmp.wav"});
+        //rubberband.run(new String[]{"--tempo", Float.toString(targetBpm / info.bpm), "/tmp/tmp.wav", "/Users/jackfarrelly/Downloads/out.wav"});
+    }
+}
--- src/main/java/com/jackpf/technosync/Main.java
+++ src/main/java/com/jackpf/technosync/Main.java
@@ -4,8 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableView;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by jackfarrelly on 23/01/2016.
@@ -16,35 +17,15 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
-        try {
-            String f = "/Users/jackfarrelly/Downloads/secluded.wav",
-                f2 = "/Users/jackfarrelly/Downloads/secluded-new.wav";
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/layout/main.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+        controller.initialise(stage);
 
-            float targetBpm = 130;
+        stage.setTitle("Techno Sync");
+        stage.setScene(new Scene(root, 300, 275));
 
-            Executor ffmpeg = new Executor("ffmpeg"),
-                rubberband = new Executor("rubberband");
-
-            //TrackAnalyzer analyzer = new TrackAnalyzer(new String[]{f, "-w", "-o", "/Users/jackfarrelly/results.txt"});
-            //TrackAnalyzer.Info info = analyzer.analyzeTrack(f, false);
-
-            //System.out.println(info.filename + ": " + info.bpm + "bpm");
-
-            Parent root = FXMLLoader.load(getClass().getResource("/layout/main.fxml"));
-
-            stage.setTitle("Techno Sync");
-            stage.setScene(new Scene(root, 300, 275));
-
-            TableView tableView = (TableView) root.lookup("#tracks");
-            tableView.setEditable(true);
-
-            stage.show();
-
-            //ffmpeg.run(new String[]{"-i", info.filename, "/tmp/tmp.wav"});
-            //rubberband.run(new String[]{"--tempo", Float.toString(targetBpm / info.bpm), "/tmp/tmp.wav", "/Users/jackfarrelly/Downloads/out.wav"});
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        stage.show();
     }
 }
