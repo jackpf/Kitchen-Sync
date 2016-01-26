96b30a1ddc443e63962bdac15a31f1cdc57d9d77
Jack Farrelly <jack.farrelly@deepskyblue.com>
2016-01-26 17:24:01 +0000

    Smart run button

--- src/main/java/com/jackpf/kitchensync/Controller.java
+++ src/main/java/com/jackpf/kitchensync/Controller.java
@@ -8,10 +8,7 @@ import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ProgressIndicator;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -21,6 +18,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by jackfarrelly on 25/01/2016.
@@ -50,6 +48,12 @@ public class Controller {
     public void initialise(Stage stage) {
         this.stage = stage;
         bpm.setCellFactory(TextFieldTableCell.<Info>forTableColumn());
+        bpm.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Info, String>>() {
+            public void handle(TableColumn.CellEditEvent<Info, String> event) {
+                event.getRowValue().setBpm(event.getNewValue());
+                enableRunButtonIfReady();
+            }
+        });
 
         ffmpeg = new Executor("ffmpeg");
         rubberband = new Executor("rubberband");
@@ -77,6 +81,21 @@ public class Controller {
         }
     }
 
+    protected void enableRunButtonIfReady() {
+        ObservableList<Info> data = tracks.getItems();
+        boolean isRunnable = true;
+
+        try {
+            for (Info track : data) {
+                Integer.parseInt(track.getBpm());
+            }
+        } catch (NumberFormatException e) {
+            isRunnable = false;
+        }
+
+        runButton.setDisable(!isRunnable);
+    }
+
     protected void addTrack(File file) {
         final ObservableList<Info> data = tracks.getItems();
 
@@ -89,12 +108,14 @@ public class Controller {
         serviceExample.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
             public void handle(WorkerStateEvent workerStateEvent) {
                 TrackAnalyzer.Info info = serviceExample.getValue();
+
                 for (Info track : data) {
                     if (track.getFilename().equals(info.filename)) {
                         track.setBpm(Integer.toString(info.bpm));
-                        continue;
                     }
                 }
+
+                enableRunButtonIfReady();
             }
         });
 
@@ -108,10 +129,6 @@ public class Controller {
         fileChooser.getExtensionFilters().add(extFilter);
         List<File> files = fileChooser.showOpenMultipleDialog(stage);
 
-        if (files.size() > 0) {
-            runButton.setDisable(false);
-        }
-
         for (File file : files) {
             addTrack(file);
         }
@@ -119,6 +136,16 @@ public class Controller {
 
     @FXML
     protected void run(ActionEvent event) {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Target BPM");
+        dialog.setHeaderText("Set target BPM");
+        dialog.setContentText("Enter target BPM:");
+
+        Optional<String> result = dialog.showAndWait();
+        if (result.isPresent()){
+            System.out.println("Target BPM: " + result.get());
+        }
+
         //ffmpeg.run(new String[]{"-i", info.filename, "/tmp/tmp.wav"});
         //rubberband.run(new String[]{"--tempo", Float.toString(targetBpm / info.bpm), "/tmp/tmp.wav", "/Users/jackfarrelly/Downloads/out.wav"});
     }
