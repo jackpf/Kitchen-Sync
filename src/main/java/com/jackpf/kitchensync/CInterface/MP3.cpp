db9f088bea4eba4d6967a01538e7cdb3248760e5
Jack Farrelly <jack.farrelly@deepskyblue.com>
2016-02-18 12:36:16 +0000

    Working fft :D

--- src/main/java/com/jackpf/kitchensync/CInterface/MP3.cpp
+++ src/main/java/com/jackpf/kitchensync/CInterface/MP3.cpp
@@ -1,76 +1,50 @@
-#include <math.h>
-#include <fstream>
 #include <iostream>
-#include <vector>
-extern "C" {
-    #include <libavutil/opt.h>
-    #include <libavcodec/avcodec.h>
-    #include <libavutil/channel_layout.h>
-    #include <libavutil/common.h>
-    #include <libavutil/imgutils.h>
-    #include <libavutil/mathematics.h>
-    #include <libavutil/samplefmt.h>
-}
 
-#define INBUF_SIZE 4096
-#define AUDIO_INBUF_SIZE 20480
-#define AUDIO_REFILL_THRESH 4096
+#include <fftw3.h>
+#include <soundtouch/SoundTouch.h>
+#include "include/WavFile.h"
 
-AVCodecContext *cntx_;
+using namespace std;
+using namespace soundtouch;
 
-AVCodec *codec_;
+#define BUFF_SIZE           160000
 
-std::vector<unsigned char>  decode(unsigned char* data, unsigned int len)
-{
-    std::vector<unsigned char> ret;
-
-    AVPacket avpkt;
-    av_init_packet(&avpkt);
-    avpkt.data = data;
-    avpkt.size = len;
-
-    AVFrame* pframe = av_frame_alloc();
-    while (avpkt.size > 0){
-        int goted = -1;av_frame_unref(pframe);
-        int used = avcodec_decode_audio4(cntx_, pframe, &goted, &avpkt);
-        if (goted){
-            ret.insert(ret.end(), pframe->data[0], pframe->data[0] + pframe->linesize[0]);
-            avpkt.data += used;
-            avpkt.size -= used;
-            avpkt.dts = avpkt.pts = AV_NOPTS_VALUE;
-        }
-        else if (goted == 0){
-            avpkt.data += used;
-            avpkt.size -= used;
-            avpkt.dts = avpkt.pts = AV_NOPTS_VALUE;
-        }
-        else if(goted < 0){
-            break;
-        }
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        cout << "Incorrect args" << endl;
+        return -1;
     }
-    av_frame_free(&pframe);
-    return ret;
-}
 
-int main(int argc, char **argv)
-{
-avcodec_register_all();
+    SAMPLETYPE sampleBuffer[BUFF_SIZE];
+    WavInFile inFile(argv[1]);
 
-cntx_ = new AVCodecContext();
-codec_ = avcodec_find_encoder(AV_CODEC_ID_MP2);
+    fftw_complex *in, *out;
+    fftw_plan p;
 
-cntx_->channels = 1;
-cntx_->sample_rate = 44100;
-cntx_->sample_fmt = AV_SAMPLE_FMT_S16P;
-cntx_->channel_layout =  AV_CH_LAYOUT_STEREO;
-cntx_->bit_rate = 16000;
-int err_ = avcodec_open2(cntx_, codec_, NULL);
+    in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * BUFF_SIZE);
+    out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * BUFF_SIZE);
 
-    std::ifstream ifs(argv[1]);
-      std::string content( (std::istreambuf_iterator<char>(ifs) ),
-                           (std::istreambuf_iterator<char>()    ) );
+    size_t samplesRead = inFile.read(sampleBuffer, BUFF_SIZE);
 
-    decode((unsigned char *)content.c_str(), content.length());
+    cout << "Read " << samplesRead << " samples" << endl;
 
-    return 0;
-}
+    //...
+    p = fftw_plan_dft_1d(BUFF_SIZE, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+
+    for (int i = 0; i < BUFF_SIZE; i++) {
+        in[i][0] = sampleBuffer[i];
+        //cout << "in[" << i << "][0] = " << sampleBuffer[i] << endl;
+    }
+
+    //...
+    fftw_execute(p); /* repeat as needed */
+    //...
+    fftw_destroy_plan(p);
+    fftw_free(in); fftw_free(out);
+
+    cout << "Done" << endl;
+
+    for (int i = 0; i < BUFF_SIZE; i++) {
+        cout << i << ": " << out[i][0] << ", " << out[i][1] << endl;
+    }
+ }
