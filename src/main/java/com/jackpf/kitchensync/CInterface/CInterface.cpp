57f42d4d657b0051c007847ed16351145c5d1429
Jack Farrelly <jack.farrelly@deepskyblue.com>
2016-02-23 11:21:24 +0000

    JNI exception handling

--- src/main/java/com/jackpf/kitchensync/CInterface/CInterface.cpp
+++ src/main/java/com/jackpf/kitchensync/CInterface/CInterface.cpp
@@ -1,8 +1,20 @@
 #include <jni.h>
 #include <stdio.h>
+#include <iostream>
 
 #include "com_jackpf_kitchensync_CInterface_CInterface.h"
 #include "KitchenSync.h"
+
+static void throwException(JNIEnv *env, const std::runtime_error &e) {
+    const char pre[] = "Kitchen Sync error : ";
+    char mBuf[strlen(pre) + strlen(e.what()) + 1];
+
+    sprintf(mBuf, "%s%s", pre, e.what());
+
+    fprintf(stderr, "%s\n", mBuf);
+
+    env->ThrowNew(env->FindClass("java/lang/Exception"), mBuf);
+}
 
 JNIEXPORT jstring JNICALL Java_com_jackpf_kitchensync_CInterface_CInterface_getVersion
   (JNIEnv *env, jobject obj)
@@ -13,9 +25,16 @@ JNIEXPORT jstring JNICALL Java_com_jackpf_kitchensync_CInterface_CInterface_getV
 JNIEXPORT jfloat JNICALL Java_com_jackpf_kitchensync_CInterface_CInterface_getBpm
   (JNIEnv *env, jobject obj, jstring jFilename)
 {
-    const char *filename = env->GetStringUTFChars(jFilename, 0);
+    const char *filename;
+    float bpm;
 
-    float bpm = KitchenSync::getBpm(filename);
+    filename = env->GetStringUTFChars(jFilename, 0);
+
+    try {
+        bpm = KitchenSync::getBpm(filename);
+    } catch (const std::runtime_error &e) {
+        throwException(env, e);
+    }
 
     env->ReleaseStringUTFChars(jFilename, filename);
 
@@ -25,10 +44,17 @@ JNIEXPORT jfloat JNICALL Java_com_jackpf_kitchensync_CInterface_CInterface_getBp
 JNIEXPORT void JNICALL Java_com_jackpf_kitchensync_CInterface_CInterface_setBpm
   (JNIEnv *env, jobject obj, jstring jInFilename, jstring jOutFilename, jfloat fromBpm, jfloat toBpm)
 {
-    const char *inFilename = env->GetStringUTFChars(jInFilename, 0);
-    const char *outFilename = env->GetStringUTFChars(jOutFilename, 0);
+    const char *inFilename;
+    const char *outFilename;
 
-    KitchenSync::setBpm(inFilename, outFilename, (float) fromBpm, (float) toBpm);
+    inFilename = env->GetStringUTFChars(jInFilename, 0);
+    outFilename = env->GetStringUTFChars(jOutFilename, 0);
+
+    try {
+        KitchenSync::setBpm(inFilename, outFilename, (float) fromBpm, (float) toBpm);
+    } catch (const std::runtime_error &e) {
+        throwException(env, e);
+    }
 
     env->ReleaseStringUTFChars(jInFilename, inFilename);
     env->ReleaseStringUTFChars(jOutFilename, outFilename);
@@ -37,9 +63,16 @@ JNIEXPORT void JNICALL Java_com_jackpf_kitchensync_CInterface_CInterface_setBpm
 JNIEXPORT jfloat JNICALL Java_com_jackpf_kitchensync_CInterface_CInterface_getQuality
   (JNIEnv *env, jobject obj, jstring jFilename)
 {
-    const char *filename = env->GetStringUTFChars(jFilename, 0);
+    const char *filename;
+    float quality;
 
-    float quality = KitchenSync::getQuality(filename);
+    filename = env->GetStringUTFChars(jFilename, 0);
+
+    try {
+        quality = KitchenSync::getQuality(filename);
+    } catch (const std::runtime_error &e) {
+        throwException(env, e);
+    }
 
     env->ReleaseStringUTFChars(jFilename, filename);
 
--- src/main/java/com/jackpf/kitchensync/CInterface/test/Test.cpp
+++ src/main/java/com/jackpf/kitchensync/CInterface/test/Test.cpp
@@ -4,26 +4,40 @@
 #include "KitchenSync.h"
 #include "KitchenSyncAnalyser.h"
 
+static void throwException(const std::runtime_error &e) {
+    const char pre[] = "Kitchen Sync error : ";
+    char mBuf[strlen(pre) + strlen(e.what()) + 1];
+
+    sprintf(mBuf, "%s%s", pre, e.what());
+
+    printf("%s\n", mBuf);
+    exit(-1);
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 4) {
         fprintf(stderr, "Usage: kstest <inFile> <outFile> <targetBpm>\n");
         return -1;
     }
 
-    const char *version = KitchenSync::getVersion();
-    printf("Soundtouch lib version: %s\n", version);
+    try {
+        const char *version = KitchenSync::getVersion();
+        printf("Soundtouch lib version: %s\n", version);
 
-    float bpm = KitchenSync::getBpm(argv[1]);
+        float bpm = KitchenSync::getBpm(argv[1]);
 
-    printf("BPM of %s: %f'n\n", argv[1], bpm);
+        printf("BPM of %s: %f'n\n", argv[1], bpm);
 
-    float quality = KitchenSync::getQuality(argv[1]);
+        float quality = KitchenSync::getQuality(argv[1]);
 
-    printf("Quality of %s: %f\n", argv[1], quality);
+        printf("Quality of %s: %f\n", argv[1], quality);
 
-    KitchenSync::setBpm(argv[1], argv[2], bpm, std::stof(argv[3]));
+        KitchenSync::setBpm(argv[1], argv[2], bpm, std::stof(argv[3]));
 
-    printf("Wrote to %s\n", argv[2]);
+        printf("Wrote to %s\n", argv[2]);
+    } catch (const std::runtime_error &e) {
+        throwException(e);
+    }
 
     return 0;
 }
