c326255a07633dc417686316a5826df04bd0e240
Jack Farrelly <jack.farrelly@deepskyblue.com>
2016-02-23 11:21:33 +0000

    Refactored KitchenSync

--- src/main/java/com/jackpf/kitchensync/CInterface/KitchenSync.cpp
+++ src/main/java/com/jackpf/kitchensync/CInterface/KitchenSync.cpp
@@ -10,85 +10,16 @@ namespace KitchenSync {
 
         *inFile = new WavInFile(params->inFileName);
 
-        // ... open output file with same sound parameters
         bits = (int) (*inFile)->getNumBits();
         samplerate = (int) (*inFile)->getSampleRate();
         channels = (int) (*inFile)->getNumChannels();
 
         if (params->outFileName && *outFile != nullptr) {
             *outFile = new WavOutFile(params->outFileName, samplerate, bits, channels);
-        } else {
-            *outFile = NULL;
         }
     }
 
-    float getBpm(const char *filename) {
-        float bpmValue;
-        int nChannels;
-        WavInFile inFile(filename);
-        BPMDetect bpm(inFile.getNumChannels(), inFile.getSampleRate());
-        SAMPLETYPE sampleBuffer[BUFF_SIZE];
-
-        // detect bpm rate
-        printf("Detecting BPM rate...\n");
-
-        nChannels = (int) inFile.getNumChannels();
-        assert(BUFF_SIZE % nChannels == 0);
-
-        // Process the 'inFile' in small blocks, repeat until whole file has
-        // been processed
-        while (inFile.eof() == 0) {
-            int num, samples;
-
-            // Read sample data from input file
-            num = inFile.read(sampleBuffer, BUFF_SIZE);
-
-            // Enter the new samples to the bpm analyzer class
-            samples = num / nChannels;
-            bpm.inputSamples(sampleBuffer, samples);
-        }
-
-        // Now the whole song data has been analyzed. Read the resulting bpm.
-        bpmValue = bpm.getBpm();
-        printf("Done!\n");
-
-        // rewind the file after bpm detection
-        inFile.rewind();
-
-        if (bpmValue > 0) {
-            printf("Detected BPM rate %.1f\n\n", bpmValue);
-        } else {
-            printf("Couldn't detect BPM rate.\n\n");
-        }
-
-        return bpmValue;
-    }
-
-    static void setup(SoundTouch *pSoundTouch, const WavInFile *inFile, const RunParameters *params) {
-        int sampleRate;
-        int channels;
-
-        sampleRate = (int) inFile->getSampleRate();
-        channels = (int) inFile->getNumChannels();
-        pSoundTouch->setSampleRate(sampleRate);
-        pSoundTouch->setChannels(channels);
-
-        pSoundTouch->setTempoChange(params->tempoDelta);
-        pSoundTouch->setPitchSemiTones(params->pitchDelta);
-        pSoundTouch->setRateChange(params->rateDelta);
-
-        pSoundTouch->setSetting(SETTING_USE_QUICKSEEK, params->quick);
-        pSoundTouch->setSetting(SETTING_USE_AA_FILTER, !(params->noAntiAlias));
-
-        if (params->speech) {
-            // use settings for speech processing
-            pSoundTouch->setSetting(SETTING_SEQUENCE_MS, 40);
-            pSoundTouch->setSetting(SETTING_SEEKWINDOW_MS, 15);
-            pSoundTouch->setSetting(SETTING_OVERLAP_MS, 8);
-            fprintf(stderr, "Tune processing parameters for speech processing.\n");
-        }
-
-        // print processing information
+    static void printInfo(const RunParameters *params) {
         if (params->outFileName) {
     #ifdef SOUNDTOUCH_INTEGER_SAMPLES
             fprintf(stderr, "Uses 16bit integer sample type in processing.\n\n");
@@ -98,6 +29,9 @@ namespace KitchenSync {
         #endif
             fprintf(stderr, "Uses 32bit floating point sample type in processing.\n\n");
     #endif
+            if (params->speech) {
+                fprintf(stderr, "Tune processing parameters for speech processing.\n");
+            }
             // print processing information only if outFileName given i.e. some processing will happen
             fprintf(stderr, "Processing the file with the following changes:\n");
             fprintf(stderr, "  tempo change = %+g %%\n", params->tempoDelta);
@@ -112,28 +46,98 @@ namespace KitchenSync {
         fflush(stderr);
     }
 
-    static void process(SoundTouch *pSoundTouch, WavInFile *inFile, WavOutFile *outFile) {
+    float getBpm(const char *filename) {
+        float bpm;
+        int nChannels;
+        
+        RunParameters *params;
+        WavInFile *inFile;
+        BPMDetect *bpmDetector;
+        SAMPLETYPE sampleBuffer[BUFF_SIZE];
+
+        params = new RunParameters();
+        params->inFileName = filename;
+        openFiles(&inFile, nullptr, params);
+        bpmDetector = new BPMDetect(inFile->getNumChannels(), inFile->getSampleRate());
+
+        fprintf(stderr, "Detecting BPM rate...\n");
+
+        nChannels = inFile->getNumChannels();
+        assert(BUFF_SIZE % nChannels == 0);
+
+        while (inFile->eof() == 0) {
+            int num, samples;
+
+            num = inFile->read(sampleBuffer, BUFF_SIZE);
+            samples = num / nChannels;
+
+            bpmDetector->inputSamples(sampleBuffer, samples);
+        }
+
+        bpm = bpmDetector->getBpm();
+        inFile->rewind();
+
+        fprintf(stderr, "Done!\n");
+
+        if (bpm > 0.0) {
+            printf("Detected BPM rate %.1f\n\n", bpm);
+        } else {
+            printf("Couldn't detect BPM rate.\n\n");
+        }
+
+        delete params;
+        delete inFile;
+        delete bpmDetector;
+
+        return bpm;
+    }
+
+    static void setup(SoundTouch *soundTouch, const WavInFile *inFile, const RunParameters *params) {
+        int sampleRate;
+        int channels;
+
+        sampleRate = (int) inFile->getSampleRate();
+        channels = (int) inFile->getNumChannels();
+
+        soundTouch->setSampleRate(sampleRate);
+        soundTouch->setChannels(channels);
+
+        soundTouch->setTempoChange(params->tempoDelta);
+        soundTouch->setPitchSemiTones(params->pitchDelta);
+        soundTouch->setRateChange(params->rateDelta);
+
+        soundTouch->setSetting(SETTING_USE_QUICKSEEK, params->quick);
+        soundTouch->setSetting(SETTING_USE_AA_FILTER, !(params->noAntiAlias));
+
+        if (params->speech) {
+            soundTouch->setSetting(SETTING_SEQUENCE_MS, 40);
+            soundTouch->setSetting(SETTING_SEEKWINDOW_MS, 15);
+            soundTouch->setSetting(SETTING_OVERLAP_MS, 8);
+        }
+
+        printInfo(params);
+    }
+
+    static void process(SoundTouch *soundTouch, WavInFile *inFile, WavOutFile *outFile) {
         int nSamples;
         int nChannels;
         int buffSizeSamples;
         SAMPLETYPE sampleBuffer[BUFF_SIZE];
 
-        if ((inFile == NULL) || (outFile == NULL)) return;  // nothing to do.
+        assert(inFile != nullptr);
+        assert(outFile != nullptr);
 
-        nChannels = (int) inFile->getNumChannels();
+        nChannels = inFile->getNumChannels();
         assert(nChannels > 0);
         buffSizeSamples = BUFF_SIZE / nChannels;
 
-        // Process samples read from the input file
         while (inFile->eof() == 0) {
             int num;
 
-            // Read a chunk of samples from the input file
             num = inFile->read(sampleBuffer, BUFF_SIZE);
             nSamples = num / (int) inFile->getNumChannels();
 
-            // Feed the samples into SoundTouch processor
-            pSoundTouch->putSamples(sampleBuffer, nSamples);
+            soundTouch->putSamples(sampleBuffer, nSamples);
 
             // Read ready samples from SoundTouch processor & write them output file.
             // NOTES:
@@ -145,16 +149,16 @@ namespace KitchenSync {
             //   outputs samples.
             do
             {
-                nSamples = pSoundTouch->receiveSamples(sampleBuffer, buffSizeSamples);
+                nSamples = soundTouch->receiveSamples(sampleBuffer, buffSizeSamples);
                 outFile->write(sampleBuffer, nSamples * nChannels);
             } while (nSamples != 0);
         }
 
         // Now the input file is processed, yet 'flush' few last samples that are
         // hiding in the SoundTouch's internal processing pipeline.
-        pSoundTouch->flush();
+        soundTouch->flush();
         do {
-            nSamples = pSoundTouch->receiveSamples(sampleBuffer, buffSizeSamples);
+            nSamples = soundTouch->receiveSamples(sampleBuffer, buffSizeSamples);
             outFile->write(sampleBuffer, nSamples * nChannels);
         } while (nSamples != 0);
     }
@@ -163,41 +167,31 @@ namespace KitchenSync {
         WavInFile *inFile;
         WavOutFile *outFile;
         RunParameters *params;
-        SoundTouch soundTouch;
+        SoundTouch *soundTouch;
 
-        try
-        {
-            // Parse command line parameters
-            params = new RunParameters();
+        soundTouch = new SoundTouch();
+        params = new RunParameters();
 
-            params->inFileName = inFilename;
-            params->outFileName = outFilename;
-            params->rateDelta = (toBpm / fromBpm - 1.0f) * 100.0f;
+        params->inFileName = inFilename;
+        params->outFileName = outFilename;
+        params->rateDelta = (toBpm / fromBpm - 1.0f) * 100.0f;
 
-            // Open input & output files
-            openFiles(&inFile, &outFile, params);
+        openFiles(&inFile, &outFile, params);
 
-            // Setup the 'SoundTouch' object for processing the sound
-            setup(&soundTouch, inFile, params);
+        setup(soundTouch, inFile, params);
 
-            // clock_t cs = clock();    // for benchmarking processing duration
-            // Process the sound
-            process(&soundTouch, inFile, outFile);
-            // clock_t ce = clock();    // for benchmarking processing duration
-            // printf("duration: %lf\n", (double)(ce-cs)/CLOCKS_PER_SEC);
+        // clock_t cs = clock();    // for benchmarking processing duration
+        // Process the sound
+        process(soundTouch, inFile, outFile);
+        // clock_t ce = clock();    // for benchmarking processing duration
+        // printf("duration: %lf\n", (double)(ce-cs)/CLOCKS_PER_SEC);
 
-            // Close WAV file handles & dispose of the objects
-            delete inFile;
-            delete outFile;
-            delete params;
+        delete inFile;
+        delete outFile;
+        delete params;
+        delete soundTouch;
 
-            fprintf(stderr, "Done!\n");
-        }
-        catch (const std::runtime_error &e)
-        {
-            // An exception occurred during processing, display an error message
-            fprintf(stderr, "%s\n", e.what());
-        }
+        fprintf(stderr, "Done!\n");
     }
 
     float getQuality(const char *filename) {
