93cbc590ab30548df201a495d4fdc6241103b505
Jack Farrelly <jack.farrelly@deepskyblue.com>
2016-02-26 13:36:17 +0000

    Formatting

--- src/main/java/com/jackpf/kitchensync/CInterface/include/FLACDecoder.cpp
+++ src/main/java/com/jackpf/kitchensync/CInterface/include/FLACDecoder.cpp
@@ -3,12 +3,12 @@
 FLACDecoder::FLACDecoder(const char *filename) : FLAC::Decoder::File() {
     set_md5_checking(true);
 
-	FLAC__StreamDecoderInitStatus init_status = init(filename);
-	if(init_status != FLAC__STREAM_DECODER_INIT_STATUS_OK) {
-		throw std::runtime_error(std::string("ERROR: initializing decoder: ") + std::string(FLAC__StreamDecoderInitStatusString[init_status]));
-	}
+    FLAC__StreamDecoderInitStatus init_status = init(filename);
+    if(init_status != FLAC__STREAM_DECODER_INIT_STATUS_OK) {
+        throw std::runtime_error(std::string("ERROR: initializing decoder: ") + std::string(FLAC__StreamDecoderInitStatusString[init_status]));
+    }
 
-	process_until_end_of_stream();
+    process_until_end_of_stream();
 }
 
 void FLACDecoder::write_little_endian_uint16(FLAC__uint16 x)
@@ -46,44 +46,44 @@ uint FLACDecoder::getNumSamples() const {
 
 ::FLAC__StreamDecoderWriteStatus FLACDecoder::write_callback(const ::FLAC__Frame *frame, const FLAC__int32 * const buffer[])
 {
-	size_t i;
+    size_t i;
 
-	if(total_samples == 0) {
-		fprintf(stderr, "ERROR: this example only works for FLAC files that have a total_samples count in STREAMINFO\n");
-		return FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
-	}
-	if(channels != 2 || bps != 16) {
-		fprintf(stderr, "ERROR: this example only supports 16bit stereo streams\n");
-		return FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
-	}
+    if(total_samples == 0) {
+        fprintf(stderr, "ERROR: this example only works for FLAC files that have a total_samples count in STREAMINFO\n");
+        return FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
+    }
+    if(channels != 2 || bps != 16) {
+        fprintf(stderr, "ERROR: this example only supports 16bit stereo streams\n");
+        return FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
+    }
 
-	/* write decoded PCM samples */
-	for(i = 0; i < frame->header.blocksize; i++) {
-	    write_little_endian_uint16((FLAC__uint16)buffer[0][i]);  /* left channel */
-	    write_little_endian_uint16((FLAC__uint16)buffer[1][i]);  /* right channel */
-	}
+    /* write decoded PCM samples */
+    for(i = 0; i < frame->header.blocksize; i++) {
+        write_little_endian_uint16((FLAC__uint16)buffer[0][i]);  /* left channel */
+        write_little_endian_uint16((FLAC__uint16)buffer[1][i]);  /* right channel */
+    }
 
-	return FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE;
+    return FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE;
 }
 
 void FLACDecoder::metadata_callback(const ::FLAC__StreamMetadata *metadata)
 {
-	/* print some stats */
-	if(metadata->type == FLAC__METADATA_TYPE_STREAMINFO) {
-		/* save for later */
-		total_samples = metadata->data.stream_info.total_samples;
-		sample_rate = metadata->data.stream_info.sample_rate;
-		channels = metadata->data.stream_info.channels;
-		bps = metadata->data.stream_info.bits_per_sample;
+    /* print some stats */
+    if(metadata->type == FLAC__METADATA_TYPE_STREAMINFO) {
+        /* save for later */
+        total_samples = metadata->data.stream_info.total_samples;
+        sample_rate = metadata->data.stream_info.sample_rate;
+        channels = metadata->data.stream_info.channels;
+        bps = metadata->data.stream_info.bits_per_sample;
 
-		fprintf(stderr, "sample rate    : %u Hz\n", sample_rate);
-		fprintf(stderr, "channels       : %u\n", channels);
-		fprintf(stderr, "bits per sample: %u\n", bps);
-		fprintf(stderr, "total samples  : %llu\n", total_samples);
-	}
+        fprintf(stderr, "sample rate    : %u Hz\n", sample_rate);
+        fprintf(stderr, "channels       : %u\n", channels);
+        fprintf(stderr, "bits per sample: %u\n", bps);
+        fprintf(stderr, "total samples  : %llu\n", total_samples);
+    }
 }
 
 void FLACDecoder::error_callback(::FLAC__StreamDecoderErrorStatus status)
 {
-	fprintf(stderr, "Got error callback: %s\n", FLAC__StreamDecoderErrorStatusString[status]);
+    fprintf(stderr, "Got error callback: %s\n", FLAC__StreamDecoderErrorStatusString[status]);
 }
